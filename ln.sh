08ad6bea6251f706045ed22fb0b69653003c368b
Tonvin Tian <itonvin@gmail.com>
2022-11-24 08:22:18 +0000

    rename project

--- ln.sh
+++ ln.sh
@@ -3,23 +3,23 @@
 root="/root/"
 
 rm -i $root".vimrc"
-ln -s $root"MelonRC/vim/vimrc" $root".vimrc"
+ln -s $root"dotrc/vim/vimrc" $root".vimrc"
 
 mkdir -p $root".vim/colors/"
-cp $root"MelonRC/vim/melon.vim" $root".vim/colors/"
+cp $root"dotrc/vim/melon.vim" $root".vim/colors/"
 
 rm -i $root".zshrc"
-ln -s $root"MelonRC/zsh/zshrc" $root".zshrc"
+ln -s $root"dotrc/zsh/zshrc" $root".zshrc"
 
 rm $root".screenrc"
-ln -s $root"MelonRC/screen/screenrc" $root".screenrc"
+ln -s $root"dotrc/screen/screenrc" $root".screenrc"
 
 rm -i $root".gitconfig"
-ln -s $root"MelonRC/git/gitconfig" $root".gitconfig"
+ln -s $root"dotrc/git/gitconfig" $root".gitconfig"
 
-cp $root"MelonRC/grep/exclude" $root".grep-exclude"
-cp $root"MelonRC/grep/exclude-dir" $root".grep-exclude-dir"
+cp $root"dotrc/grep/exclude" $root".grep-exclude"
+cp $root"dotrc/grep/exclude-dir" $root".grep-exclude-dir"
 
-cp $root"MelonRC/zsh/alias" $root".alias"
+cp $root"dotrc/zsh/alias" $root".alias"
 
 source $root".zshrc"
