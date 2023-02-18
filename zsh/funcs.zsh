baac11f9449a44a764065af33be7ff8045ab3f69
Tonvin Tian <itonvin@gmail.com>
2023-02-18 15:15:06 +0800

    nocommit

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -168,27 +168,6 @@ function rm0() {
     fi
 }
 
-# map vim to nvim first vim second
-function vim() {
-    if [ ! -z $(command -v nvim) ]
-    then
-        echo "neovim $@"
-        sleep 0.5
-        nvim "$@"
-    else
-        if [ ! -z $(command -v vim) ]
-        then
-            echo "vim $@"
-            sleep 0.5
-            vim "$@"
-        else
-            echo "please install vim"
-        fi
-    fi
-}
-
-
-
 function ins(){
     if [[ ! -z $1 ]]
         then
